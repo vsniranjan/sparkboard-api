3385261058ad6174680061d93fabf6bed551db3a
Niranjan V S <niranjan.veluthamessery@gmail.com>
2025-09-24 16:46:55 +0530

    Used nodemon and cleaned routes

--- server.js
+++ server.js
@@ -3,51 +3,11 @@ const port = 5000;
 
 const app = express();
 
-const ideas = [
-  {
-    id: 1,
-    text: "Positive Newsletter, a newsletter that only shares positive, uplifting news",
-    tag: "Technology",
-    username: "TonyStark",
-    date: "2022-01-02",
-  },
-  {
-    id: 2,
-    text: "Milk cartons that turn a different color the older that your milk is getting",
-    tag: "Inventions",
-    username: "SteveRogers",
-    date: "2022-01-02",
-  },
-  {
-    id: 3,
-    text: "ATM location app which lets you know where the closest ATM is and if it is in service",
-    tag: "Software",
-    username: "BruceBanner",
-    date: "2022-01-02",
-  },
-];
-
 app.get("/", (req, res) => {
-  res.send("Main");
+  res.send({ message: "Sparkboard API" });
 });
 
-// Get all ideas
-app.get("/api/ideas", (req, res) => {
-  res.send({ success: true, data: ideas });
-});
-
-// Getting a specific idea
-
-app.get("/api/ideas/:id", (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id);
-
-  if (!idea) {
-    return res
-      .status(404)
-      .json({ success: false, error: "Resource not found" });
-  }
-
-  res.send({ success: true, data: idea });
-});
+const ideasRouter = require("./routes/ideas");
+app.use("/api/ideas", ideasRouter);
 
 app.listen(port, () => console.log(`server listening on port ${port}`));
