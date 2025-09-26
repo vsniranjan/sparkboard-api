500338c344cc4367802c5166645b2c0d879fff6a
Niranjan V S <niranjan.veluthamessery@gmail.com>
2025-09-26 17:21:46 +0530

    Put and Delete requests

--- routes/ideas.js
+++ routes/ideas.js
@@ -58,4 +58,33 @@ router.post("/", (req, res) => {
   res.json({ sucess: true, data: idea });
 });
 
+// Update Idea
+router.put("/:id", (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: "Resource not found" });
+  }
+
+  idea.text = req.body.text || idea.text;
+  idea.tag = req.body.tag || idea.tag;
+  res.send({ success: true, data: idea });
+});
+
+// Delete Idea
+router.delete("/:id", (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: "Resource not found" });
+  }
+
+  ideas.splice(ideas.indexOf(idea), 1);
+  res.send({ success: true, data: ideas });
+});
+
 module.exports = router;
